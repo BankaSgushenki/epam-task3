0e550f18766bafc74bfbbb2707558f19327686f1
Kostya Sergeev <kostyasergeev93@gmail.com>
2014-05-04 11:33:55 +0300

    animations

--- script.js
+++ script.js
@@ -2,18 +2,17 @@ var currentWindowWidth = 0;
 
 var yotubeCollection;
 
-window.onresize =  function()
-{
+window.onresize =  function () {
     if (self.innerWidth - currentWindowWidth > 300) {
         currentWindowWidth = self.innerWidth;
-        var index = (currentWindowWidth - currentWindowWidth % 300)/300 - 1;
-        addElement(index, window.yotubeCollection[index], 300); 
+        var index = (currentWindowWidth - currentWindowWidth % 300) / 300 - 1;
+        addElement(index, window.yotubeCollection[index], 300);
     }
 
-    if (currentWindowWidth - self.innerWidth > 300){
+    if (currentWindowWidth - self.innerWidth > 300) {
         currentWindowWidth = self.innerWidth;
         var parent = document.getElementById("content");
-        parent.removeChild(parent.lastChild); 
+        parent.removeChild(parent.lastChild);
     }
 }
 
@@ -28,8 +27,7 @@ function createComponent(className, rawYouTubeData) {
     <div class= "data">' + rawYouTubeData.publishDate + '</div> \
     <div class= "views">' + rawYouTubeData.viewCount + '</div> \
     </div>';
-    container.firstChild.onmousemove  = mouseMoveEvent;
-    container.firstChild.onmouseout  = mouseOutEvent;
+    container.firstChild.onclick  = mouseClickEvent;
 return container.firstChild;
 }
 
@@ -37,9 +35,15 @@ function createMainContent(className) {
     'use strict';
     var container = document.createElement('div');
     container.innerHTML = '<div id = "content" class="' + className + '"> \
-    <input type = "text" class = "searchBox"></input> \
-    <input type = "button" value = "Search" onclick = "test()" class = "searchButton"></input> \
-    <div class= "tooltip"></div> \
+    <input type = "text" placeholder="Youtube search" onkeyup  = "searchEvent(event)" class = "searchBox"></input> \
+    </div>';
+return container.firstChild;
+}
+
+function createToolTip(className) {
+    'use strict';
+    var container = document.createElement('div');
+    container.innerHTML = '<div class="' + className + '"> \
     </div>';
 return container.firstChild;
 }
@@ -52,33 +56,32 @@ function addElement(number, someContent , elementWidth) {
     'use strict';
     var parent = document.getElementById("content");
     var container = createComponent('container', someContent);
-    setPosition(container, (elementWidth + 10) * number);
+    setPosition(container, (elementWidth + 30) * number);
     parent.appendChild(container);
 }
 
 function convertYouTubeResponseToClipList(rawYouTubeData) {
-                var clipList = [];
-                var entries = rawYouTubeData.feed.entry;
-                if (entries) {
-                    for (var i = 0, l = entries.length; i < l; i++){
-                        var entry = entries[i];
- 
-                        var date = new Date(Date.parse(entry.updated.$t));
-                        var shortId = entry.id.$t.match(/video:.*/).toString().split(":")[1];
-                        clipList.push({
-                           id: shortId,
-                            youtubeLink: "http://www.youtube.com/embed/" + shortId,
-                            title: entry.title.$t,
-                            thumbnail: entry.media$group.media$thumbnail[1].url,
-                            description: entry.media$group.media$description.$t,
-                            author: entry.author[0].name.$t,
-                            publishDate: date.toUTCString(),
-                            viewCount: entry.yt$statistics.viewCount
-                        });
-                    }
+    var clipList = [];
+    var entries = rawYouTubeData.feed.entry;
+    if (entries) {
+        for (var i = 0, l = entries.length; i < l; i++){
+        var entry = entries[i];
+        var date = new Date(Date.parse(entry.updated.$t));
+        var shortId = entry.id.$t.match(/video:.*/).toString().split(":")[1];
+        clipList.push({
+        id: shortId,
+        youtubeLink: "http://www.youtube.com/embed/" + shortId,
+        title: entry.title.$t,
+        thumbnail: entry.media$group.media$thumbnail[1].url,
+        description: entry.media$group.media$description.$t,
+        author: entry.author[0].name.$t,
+        publishDate: date.toUTCString(),
+        viewCount: entry.yt$statistics.viewCount
+                    });
                 }
-                return clipList;
-            }
+        }
+    return clipList;
+}
 
 function httpGet(Url) {
     var script = document.createElement("script");
@@ -108,29 +111,42 @@ function removeContainer(container) {
 
 function clearAll() {
     var parent = document.getElementById("content");
-    var elements = parent.getElementsByTagName('div');
-    for(var index = 0; index < elements.length; index++) {
-       elements[index].parentNode.removeChild(elements[index]);
-       console.log(index);
+    var elements = parent.children;
+    for(var index = 1; index < elements.length; index++) {
+        elements[index].parentNode.removeChild(elements[index]);
+        parent.removeChild(elements[index]);
+        console.log(index);
+    }
+}
+
+var searchEvent = function(event) {
+    if((event.keyCode==13)||(event.keyCode==32)) {
+    clearAll();
+    httpGet(createRequestURL()); 
     }
 }
 
 var test = function() {
     clearAll();
-    httpGet(createRequestURL());   
 }
 
-var mouseMoveEvent = function(event) {
-    //event.target.parentNode.style.height = "550px";
-    //console.log("sd");
+var mouseClickEvent = function(event) {
+    var parent = document.getElementById("content");
+    var elements = parent.children;
+    for(var index = 1; index < elements.length; index++) {
+        elements[index].style.border = "solid #6c6c6c 2px";
+        elements[index].style.height = "500px";
+    }
+    event.target.parentNode.style.border = "solid grey 5px";
+    event.target.parentNode.style.height = "560px";
 }
 
-var mouseOutEvent = function(event) {
-    //event.target.parentNode.style.height = "500px";
-    //console.log("sd");
-}
 // **************** Functions declarations *********************************//
 currentWindowWidth = self.innerWidth;
 old = self.innerWidth;
+
 var content = createMainContent('content');
 document.body.appendChild(content);
+
+var tooltip = createToolTip('tooltip');
+document.body.appendChild(tooltip);
